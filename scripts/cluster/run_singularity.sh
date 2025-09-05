e9c5f94c8cccd61c1ae4863c32146e64b0cb2520
Emily Sturman <sturman@utexas.edu>
2025-09-05 11:01:00 -0500

    Fix cluster permission denied errors

--- scripts/cluster/run_singularity.sh
+++ scripts/cluster/run_singularity.sh
@@ -19,7 +19,7 @@ setup_directories() {
         "${CLUSTER_ISAAC_SIM_CACHE_DIR}/documents"; do
         if [ ! -d "$dir" ]; then
             mkdir -p "$dir"
-            echo "Created directory: $dir"
+            echo "(run_singularity.py): Created directory: $dir"
         fi
     done
 }
@@ -32,6 +32,8 @@ setup_directories() {
 
 # get script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# use unique tmpdir for each run
+JOB_TMPDIR="$TMPDIR/isaaclab_$SLURM_JOB_ID"
 
 # load variables to set the Isaac Lab path on the cluster
 source $SCRIPT_DIR/.env.cluster
@@ -39,45 +41,57 @@ source $SCRIPT_DIR/.env.wandb
 source $SCRIPT_DIR/../.env.base
 
 # make sure that all directories exists in cache directory
+mkdir "$JOB_TMPDIR"
 setup_directories
 # copy all cache files
-cp -r $CLUSTER_ISAAC_SIM_CACHE_DIR $TMPDIR
+cp -r $CLUSTER_ISAAC_SIM_CACHE_DIR $JOB_TMPDIR
 
 # make sure logs directory exists (in the permanent isaaclab directory)
 mkdir -p "$CLUSTER_ISAACLAB_DIR/logs"
 touch "$CLUSTER_ISAACLAB_DIR/logs/.keep"
 
 # copy the temporary isaaclab directory with the latest changes to the compute node
-cp -r $1 $TMPDIR
+cp -r $1 $JOB_TMPDIR
 # Get the directory name
 dir_name=$(basename "$1")
 
 # copy container to the compute node
-tar -xf $CLUSTER_SIF_PATH/$2.tar  -C $TMPDIR
+# cp $CLUSTER_SIF_PATH/$2.sif $JOB_TMPDIR
+tar -xf $CLUSTER_SIF_PATH/$2.tar -C $JOB_TMPDIR
 
 # execute command in singularity container
 # NOTE: ISAACLAB_PATH is normally set in `isaaclab.sh` but we directly call the isaac-sim python because we sync the entire
 # Isaac Lab directory to the compute node and remote the symbolic link to isaac-sim
-apptainer exec \
-    -B $TMPDIR/docker-isaac-sim/cache/kit:${DOCKER_ISAACSIM_ROOT_PATH}/kit/cache:rw \
-    -B $TMPDIR/docker-isaac-sim/cache/ov:${DOCKER_USER_HOME}/.cache/ov:rw \
-    -B $TMPDIR/docker-isaac-sim/cache/pip:${DOCKER_USER_HOME}/.cache/pip:rw \
-    -B $TMPDIR/docker-isaac-sim/cache/glcache:${DOCKER_USER_HOME}/.cache/nvidia/GLCache:rw \
-    -B $TMPDIR/docker-isaac-sim/cache/computecache:${DOCKER_USER_HOME}/.nv/ComputeCache:rw \
-    -B $TMPDIR/docker-isaac-sim/logs:${DOCKER_USER_HOME}/.nvidia-omniverse/logs:rw \
-    -B $TMPDIR/docker-isaac-sim/data:${DOCKER_USER_HOME}/.local/share/ov/data:rw \
-    -B $TMPDIR/docker-isaac-sim/documents:${DOCKER_USER_HOME}/Documents:rw \
-    -B $TMPDIR/$dir_name:/workspace/isaaclab:rw \
+apptainer -d exec \
+    -B $JOB_TMPDIR/docker-isaac-sim/cache/kit:${DOCKER_ISAACSIM_ROOT_PATH}/kit/cache:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/cache/ov:${DOCKER_USER_HOME}/.cache/ov:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/cache/pip:${DOCKER_USER_HOME}/.cache/pip:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/cache/glcache:${DOCKER_USER_HOME}/.cache/nvidia/GLCache:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/cache/computecache:${DOCKER_USER_HOME}/.nv/ComputeCache:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/logs:${DOCKER_USER_HOME}/.nvidia-omniverse/logs:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/data:${DOCKER_USER_HOME}/.local/share/ov/data:rw \
+    -B $JOB_TMPDIR/docker-isaac-sim/documents:${DOCKER_USER_HOME}/Documents:rw \
+    -B $JOB_TMPDIR/$dir_name:/workspace/isaaclab:rw \
     -B $CLUSTER_ISAACLAB_DIR/logs:/workspace/isaaclab/logs:rw \
-    --nv --writable-tmpfs --containall $TMPDIR/$2.sif \
+    --nv --writable-tmpfs --containall $JOB_TMPDIR/$2.sif \
     bash -c "export ISAACLAB_PATH=/workspace/isaaclab && export WANDB_USERNAME=$WANDB_USERNAME && export WANDB_API_KEY=$WANDB_API_KEY && cd /workspace/isaaclab && /isaac-sim/python.sh ${CLUSTER_PYTHON_EXECUTABLE} ${@:3}"
 
+EXIT_CODE=$?
+
 # copy resulting cache files back to host
-rsync -azPv $TMPDIR/docker-isaac-sim $CLUSTER_ISAAC_SIM_CACHE_DIR/..
+rsync -azPv $JOB_TMPDIR/docker-isaac-sim $CLUSTER_ISAAC_SIM_CACHE_DIR/..
+# clean up tmpdir
+rm -rf $JOB_TMPDIR
 
 # if defined, remove the temporary isaaclab directory pushed when the job was submitted
 if $REMOVE_CODE_COPY_AFTER_JOB; then
     rm -rf $1
 fi
 
-echo "(run_singularity.py): Return"
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "(run_singularity.py): Success"
+else
+    echo "(run_singularity.py): Failed with exit code $EXIT_CODE"
+fi
+
+exit $EXIT_CODE
